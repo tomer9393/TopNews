bdcaaf1e91e9e242a803a83be72c35a85bbbcc65
meitalzaguri95 <MeitalMoyal>
2021-02-27 15:04:33 +0200

    AboutUsPage

---
+++ client/client-react/src/components/aboutUsPage/MeitalTeamMember.js
@@ -0,0 +1,28 @@
+function MeitalTeamMember() 
+{
+    return (
+        <>
+            <div className="col-12 col-sm-6 col-md-3">
+                <div className="single-team-area">
+                    <div className="team-member-data">
+                        <div className="team-member-designation-social-info  align-items-cente justify-content-between">
+
+                        <img src="../img/bg-img/Meital.jpg" alt/>
+                        {/* <img src="https://scontent.ftlv6-1.fna.fbcdn.net/v/t1.0-9/93230231_10206934384916322_5398447404838027264_n.jpg?_nc_cat=101&ccb=3&_nc_sid=09cbfe&_nc_ohc=_ZYMKUxd57kAX9wltDo&_nc_ht=scontent.ftlv6-1.fna&oh=e57476e4e3c5f00b87b56c895f1325e8&oe=6059D13D" alt /> */}
+                        <h4 className="font-pt">Meital Moyal</h4>
+                            <h5 className="font-pt mb-0">Editor</h5>
+                                <div className="social-info">
+                                    {/* <a href="#"><i className="fa fa-pinterest" /></a>
+                                    <a href="#"><i className="fa fa-linkedin" /></a> */}
+                                    <a href="https://www.facebook.com/meital.zaguri1/"><i className="fa fa-facebook" /></a>
+                                    {/* <a href="#"><i className="fa fa-twitter" /></a> */}
+                                </div>
+                         </div>
+                    </div>
+                </div>
+                </div>
+        </>
+    );
+}
+
+export default MeitalTeamMember;
