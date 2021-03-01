5157eb59b28c014f293f1bad9f4f5d68afa4d5d0
Lee Lisker <Lee Lisker@DESKTOP-SQAJ41L>
2021-03-01 23:36:51 +0200

    a

--- client/client-react/src/components/homePage/main/mainContent/todayPosts/singleTodayPosts.js
+++ client/client-react/src/components/homePage/main/mainContent/todayPosts/singleTodayPosts.js
@@ -1,27 +1,54 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getCommentsByArticleID } from "../../../../../api/CommentAPI";
 
+function SingleTodayPosts(props) {
+  const article = props.article;
+  const [comments, setComments] = useState();
 
-function SingleTodayPosts() {
-    return (
-        <>
-            <div className="gazette-single-todays-post d-md-flex align-items-start mb-50">
-                <div className="todays-post-thumb">
-                    <img src="img/blog-img/3.jpg" alt />
-                </div>
-                <div className="todays-post-content">
-                    {/* Post Tag */}
-                    <div className="gazette-post-tag">
-                        <a href="#">Life</a>
-                    </div>
-                    <h3><a href="#" className="font-pt mb-2">Homeless man steals $350,000 </a></h3>
-                    <p className="gazette-post-date mb-2">March 29, 2016</p>
-                    <a href="#" className="post-total-comments">3 Comments</a>
-                    <p>Aliquam quis convallis enim. Nunc pulvinar molestie sem id blandit. Nunc venenatis
-                    interdum mollis. Aliquam finibus nulla quam, a iaculis justo finibus non.
-                Suspendisse in fermentum nunc.</p>
-                </div>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    getCommentsByArticleID(article._id)
+      .then((res) => res.data)
+      .then((res) => setComments(res));
+  }, [props]);
+
+  const getCommentsString = () => {
+    if (comments.length === 1) {
+    }
+  };
+
+  return (
+    <>
+      <div className="gazette-single-todays-post d-md-flex align-items-start mb-50">
+        <div className="todays-post-thumb">
+          <img src={article.img} alt />
+        </div>
+        <div className="todays-post-content">
+          {/* Post Tag */}
+          <div className="gazette-post-tag">
+            {/* TODO: add link to */}
+            <Link to={`Category/${article.category}`}>{article.category}</Link>
+          </div>
+          {/* TODO: Add link to */}
+          <h3>
+            <Link to={`PostDetails/${article._id}`} className="font-pt mb-2">
+              {article.title}
+            </Link>
+          </h3>
+          <p className="gazette-post-date mb-2">{article.published}</p>
+          {/* TODO: Add link to */}
+          <Link to="#" className="post-total-comments">
+            {comments
+              ? comments.length === 1
+                ? "1 comment"
+                : `${comments.length} comments`
+              : ""}
+          </Link>
+          <p>{article.body}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SingleTodayPosts;
