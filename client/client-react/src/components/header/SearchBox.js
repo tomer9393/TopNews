f1acf51c8fbc1650e2e3e49834462b68e7e3e553
Daniel Braunstein <dabrauns@microsoft.com>
2021-03-06 20:41:51 +0200

    a

--- client/client-react/src/components/header/SearchBox.js
+++ client/client-react/src/components/header/SearchBox.js
@@ -1,13 +1,40 @@
 import { useState } from "react";
-import { DropdownList } from "react-widgets";
+import "react-widgets/dist/css/react-widgets.css";
+import "react-datepicker/dist/react-datepicker.css";
+import DropdownList from "react-widgets/lib/DropdownList";
+import DatePicker from "react-datepicker";
+import { categories } from "../utils";
 
 function SerachBox(props) {
   const [title, setTitle] = useState();
-  let categories = ["orange", "red", "blue", "purple"];
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   return (
     <>
-      <input onChange={(event) => setTitle(event.target.value)} />
-      <DropdownList data={categories} />
+      {props.showSearch && (
+        <>
+          <input
+            placeholder="Search"
+            onChange={(event) => setTitle(event.target.value)}
+            style={{ marginRight: "5px" }}
+          />
+          <div style={{ width: "250px", marginRight: "5px" }}>
+            <DropdownList data={categories} />
+          </div>
+          <div style={{ zIndex: 100, marginRight: "5px" }}>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
+          </div>
+          <div style={{ zIndex: 100, marginRight: "5px" }}>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setEndDate(date)}
+            />
+          </div>
+        </>
+      )}
       <img
         src={"../../../public/img/core-img/magnifying-glass-search.png"}
         onClick={() => props.setShowSearch(!props.showSearch)}
