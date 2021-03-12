b842fd8cd0d7046df4ac5887caf84d0893295901
Matan Katz <mati340@gmail.com>
2021-03-12 23:38:42 +0200

    fixed single page contact form and added email send

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -1,14 +1,38 @@
 const contact = require('../models/contact');
 const contactService = require('../services/contact');
+const nodemailer = require("nodemailer");
 
 const createContact = async (req, res) => {
   const newContact = await contactService.createContact(req.body.fullName, req.body.email, req.body.message);
   if (!newContact) {
     return res.status(501).json("No contact created");
   }
+  sendMail(req.body.email, req.body.fullName).catch(console.error);
   res.json(newContact);
 
 };
+
+const sendMail = async (email, name) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "topnews906@gmail.com",
+      pass: "Zz12345678",
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Top News" <topnews906@gmail.com>', // sender address
+    to: email, // list of receivers
+    subject: "Thank you for contact Top News", // Subject line
+    text: "Hello " + name + "Thank you for contact TOP NEWS", // plain text body
+    html: "<p><b>Hello " + name + "</b></p> <p>Thank you for contact TOP NEWS</p>", // html body
+  });
+}
 
 const getContactByID = async (req, res) => {
   if (!req.params.id) {
