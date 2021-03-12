b3b38b61383db51d254b08af2cf5ac86aaeca221
Liraz Mizrachi <liraz981@cs.colman.ac.il>
2021-03-12 16:25:58 +0200

    Error throwing at server

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -3,61 +3,66 @@ var emitter = require('../common/emitter')
 
 var myEmitter = emitter.myEmitter
 
-const createCategory = async (req, res) =>{
+const createCategory = async (req, res) => {
     const newCategory = await categoryService.createCategory(req.body.name);
-    if(newCategory !== null)
+    if (!newCategory) {
+        return res.status(501).json("No category created");
+    }
+    if (newCategory !== null)
         myEmitter.emit('createCategory');
     res.json(newCategory);
 };
 
-const getCategoryByName = async (req, res) =>{
-    const category = await categoryService.getCategoryByName(req.params.name); 
-    if (!category)
-    {
-        return res.status(404).json({ errors : ['Category not found']});
+const getCategoryByName = async (req, res) => {
+    if (!req.params.name) {
+        res.status(400).json("Category name is required");
+    }
+    const category = await categoryService.getCategoryByName(req.params.name);
+    if (!category) {
+        return res.status(404).json("Category not found");
     }
 
     res.json(category);
 };
 
-const updateCategory = async (req, res) =>{
-    if (!req.body.name)
-    {
-        res.status(400).json({
-            message : "Category name is required"
-        });
+const updateCategory = async (req, res) => {
+    if (!req.body.name) {
+        res.status(400).json("Category name is required");
     }
 
     const category = await categoryService.updateCategory(req.params.name);
-    if (!category)
-    {
-        return res.status(404).json({errors: ['Category not found']});
+    if (!category) {
+        return res.status(404).json("Category not found");
     }
 
     res.json(category);
 };
 
-const removeCategory = async (req, res) =>{
+const removeCategory = async (req, res) => {
+    if (!req.body.name) {
+        res.status(400).json("Category name is required");
+    }
     const category = await categoryService.removeCategory(req.params.name);
-    if (!category)
-    {
-        return res.status(404).json({errors : ['Category not found']});
+    if (!category) {
+        return res.status(404).json("Category not found");
     }
     myEmitter.emit('deleteCategory');
     res.send();
 };
 
-const getAllCategories = async (req,res)=>{
+const getAllCategories = async (req, res) => {
     const categories = await categoryService.getAllCategories();
-    if (!categories)
-    {
-        return res.status(404).json({errors : ['Categories not found']});
+    if (!categories) {
+        return res.status(500).json("Category not found");
     }
     res.json(categories);
 };
 
-const getNumOfCategories = async (req,res)=>{
+const getNumOfCategories = async (req, res) => {
     const count = await categoryService.getNumOfCategories();
+    if (!count) {
+        return res.send(0);
+    }
     res.json(count);
 };
 
--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -1,57 +1,64 @@
 const contact = require('../models/contact');
 const contactService = require('../services/contact');
 
-const createContact = async (req, res) =>{
-    const newContact = await contactService.createContact(req.body.fullName, req.body.email, req.body.message);
-    if (!newContact) {
-        return res.status(404).json({ errors: ['No contact created'] });
-      }
-    res.json(newContact);
+const createContact = async (req, res) => {
+  const newContact = await contactService.createContact(req.body.fullName, req.body.email, req.body.message);
+  if (!newContact) {
+    return res.status(501).json("No contact created");
+  }
+  res.json(newContact);
 
 };
 
-const getContactByID = async (req,res) =>{
-    const contact = await contactService.getContactByID(req.params.id);
-    if (!contact) {
-        return res.status(404).json({ errors: ['contact not found'] });
-      }
-    res.json(contact);
+const getContactByID = async (req, res) => {
+  if (!req.params.id) {
+    res.status(400).json("id is required");
+  }
+  const contact = await contactService.getContactByID(req.params.id);
+  if (!contact) {
+    return res.status(404).json('Contact not found');
+  }
+  res.json(contact);
 };
 
-const UpdateContact = async (req,res)=> {
-    if (!req.params.id) {
-        res.status(400).json({
-          message: "id is required",
-        });
-      }
-    
-      const contact = await contactService.UpdateContact(req.params.id, req.body.fullName, req.body.email, req.body.message);
-      if (!contact) {
-        return res.status(404).json({ errors: ['contact not found'] });
-      }
-    
-      res.json(contact);
+const UpdateContact = async (req, res) => {
+  if (!req.params.id) {
+    res.status(400).json("id is required");
+  }
+
+  const contact = await contactService.UpdateContact(req.params.id, req.body.fullName, req.body.email, req.body.message);
+  if (!contact) {
+    return res.status(404).json('contact not found');
+  }
+
+  res.json(contact);
 };
 
 
-const DeleteContact = async (req,res)=> {
-    const contact = await contactService.DeleteContact(req.params.id);
-//params mean from the URL
-    if (!contact) {
-        return res.status(404).json({ errors: ['contact not found'] });
-    } 
-    res.send();
+const DeleteContact = async (req, res) => {
+  if (!req.params.id) {
+    res.status(400).json("id is required");
+  }
+  const contact = await contactService.DeleteContact(req.params.id);
+  //params mean from the URL
+  if (!contact) {
+    return res.status(404).json('contact not found');
+  }
+  res.send();
 };
 
 const getAllContacts = async (req, res) => {
-    const contacts = await contactService.getAllContacts();
-    res.json(contacts);
-  };
+  const contacts = await contactService.getAllContacts();
+  if (!contacts) {
+    return res.status(500).json("Contacts not found");
+  }
+  res.json(contacts);
+};
 
 module.exports = {
-    createContact,
-    getContactByID,
-    UpdateContact,
-    DeleteContact,
-    getAllContacts
+  createContact,
+  getContactByID,
+  UpdateContact,
+  DeleteContact,
+  getAllContacts
 };
